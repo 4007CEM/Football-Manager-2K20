c34d371e25d1b4ff7787e43e6641eae7c2eede79
Vincbk <55707143+Vincbk@users.noreply.github.com>
2020-02-19 17:41:52 +0000

    Add files via upload
    Premiere League players

---
+++ DBS/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <windows.h>
+//#include "gameFunc.h"
+#include "dbsSource.h"
+#include "dbsCreation.h"
+
+
+using namespace std;
+
+int main()
+{
+    /*
+    GameWindow init;
+    init.InitWindow();
+    */
+
+    Players::InitPlayers(LiverpoolSubs, InitLiverpoolSubs);
+    Players::PrintPlayers(LiverpoolSubs);
+
+
+
+    return 0;
+}
